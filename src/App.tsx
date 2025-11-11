ab0348c22bd62a2d2e582d7f97f061b0732a45e7
harryakbarntuc <harry.akbar@ntucenterprise.sg>
2025-11-11 17:42:34 +0800

    init vite

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="bg-orange-600 text-white">Contact</button>
+      <button className="bg-orange-600 text-white">Contact</button>
+      <button className="bg-orange-600 text-white">Contact</button>
+      <button className="bg-orange-600 text-white">Contact</button>
+
+      <div className="credits" data-gfe-screenshot-exclude="true">
+        A challenge by{" "}
+        <a
+          href="https://www.greatfrontend.com/projects?ref=challenges"
+          target="_blank"
+        >
+          GreatFrontEnd Projects
+        </a>
+        . Built by{" "}
+        <a
+          href="https://www.greatfrontend.com/projects/u/username"
+          target="_blank"
+        >
+          Harry Akbar Ali Munir
+        </a>
+        .
+      </div>
+    </>
+  );
+}
+
+export default App;
