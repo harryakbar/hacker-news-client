1f9b77cc8c1c05c33c4890182a3e19634be08e2e
harryakbarntuc <harry.akbar@ntucenterprise.sg>
2025-11-12 15:19:20 +0800

    add github action

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
 import "./App.css";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <button className="bg-orange-600 text-white">Contact</button>
-      <button className="bg-orange-600 text-white">Contact</button>
-      <button className="bg-orange-600 text-white">Contact</button>
-
-      <div className="credits" data-gfe-screenshot-exclude="true">
-        A challenge by{" "}
-        <a
-          href="https://www.greatfrontend.com/projects?ref=challenges"
-          target="_blank"
-        >
-          GreatFrontEnd Projects
-        </a>
-        . Built by{" "}
-        <a
-          href="https://www.greatfrontend.com/projects/u/username"
-          target="_blank"
-        >
-          Harry Akbar Ali Munir
-        </a>
-        .
+    <div className="p-0 lg:p-8 min-h-screen flex flex-col">
+      <div className="flex-grow">
+        <h1 className="text-4xl">New</h1>
+        <p className="text-neutral-400">
+          Discover the latest submissions in the Hacker News community.
+        </p>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
